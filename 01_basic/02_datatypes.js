08f5d371cd3368d3a65164dd536a4318ac20b857
FARHAN AHMAD <113082273+farahn0104@users.noreply.github.com>
2025-08-29 14:06:30 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,18 @@
+// use stricts use all JS code as Newer version
+// alert(2+2) // we are using node.js not browser
+
+/* 
+number     : that is the 2 to the power 53 9,007,199,254,740,992
+bigInt     : 
+boolean    : true/flase
+string     : "Farhan"
+null       : stand alone value
+undefined  : not assing any value 
+Symbol     : for uniqueness
+object     :
+           :
+*/
+// console.log(typeof null); // object
+// NOte: the type of 'null' is a 'Object'
+// console.log(typeof undefined); // undefined
+console.log(typeof  9907199254740992n );
