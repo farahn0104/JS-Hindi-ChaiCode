1932efbe0afe7533f55f1864923b7bdde20f15e2
FARHAN AHMAD <113082273+farahn0104@users.noreply.github.com>
2025-08-29 15:33:46 +0000

    comparison operator is done

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,63 @@
+// console.log('2' > 2);
+// console.log('02' > 1);
+
+
+/* console.log(null > 0);  // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+console.log(null <= 0); // true
+ */
+
+// let n = undefined
+// let con = Number(n)
+// console.log(con); // 0
+
+// console.log(null == null);
+// console.log(null == 0);
+// console.log(null == undefined);
+/* 
+Jab bhi >, <, >=, <= use ho (Comparison): null ko number samjho (0).
+
+null > 0 -> 0 > 0 -> false
+
+null >= 0 -> 0 >= 0 -> true
+
+Jab == use ho (Equality): null sirf null ya undefined ke saath hi true dega, baaki sab ke saath false dega.
+
+null == 0 -> false (special rule)
+*/
+
+/* 
+console.log('2' === 2); // false
+console.log('2' < 2);   // false
+console.log('2' > 2);   // false
+console.log('2' <= 2);  // true
+console.log('2' >= 2);  // true
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
