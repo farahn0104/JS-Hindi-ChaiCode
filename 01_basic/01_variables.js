1cbdc83304ae75212a0aa7075736ce1c25c28fc5
FARHAN AHMAD <113082273+farahn0104@users.noreply.github.com>
2025-08-29 14:29:43 +0000

    conversion of datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 453002100108064;
+let accountEmail = 'mdfarhankic85@gmail.com';
+var accountPassword = 'farhanasari786';
+accountCity = 'Jaipur';
+
+// accountId = 2 Assignment to Constant Variable
+accountEmail = 'mdhamdankic85@gmail.com';
+accountPassword = 'hamdanansari786';
+accountCity = 'Bengluru';
+let accountState;
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,21 @@
+// let score =true;
+
+// console.log(typeof score); // string
+
+// let valueInNumber = Number(score); 
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // NaN
+// console.log(typeof NaN); // number
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.table([isLoggedIn,booleanIsLoggedIn,typeof booleanIsLoggedIn,typeof isLoggedIn]);
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+console.table([someNumber,typeof someNumber, stringNumber,typeof stringNumber ]);
+
+
+
+
