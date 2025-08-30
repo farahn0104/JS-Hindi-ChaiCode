62aa4b348722cd14e38e55bdc6181f6896821bae
FARHAN AHMAD <113082273+farahn0104@users.noreply.github.com>
2025-08-30 13:50:49 +0000

    array part 2 is done

---
+++ 02_basic/02_array.js
@@ -0,0 +1,36 @@
+/*
+const marvel_heroes = ['Thor', 'Spiderman', 'Superman', 'AntMan', 'Iron Man']
+const dc_heroes = ['Hobo', 'Shaktiman', 'Hatim', 'Batman', 'Flash',]
+
+console.log(marvel_heroes)
+marvel_heroes.push(dc_heroes); // return multi dimensional array
+
+console.log(dc_heroes)
+
+console.log(marvel_heroes)
+
+const newArr = marvel_heroes.concat(dc_heroes); // return a single array
+console.log(newArr);
+
+const all_new_heroes = [...marvel_heroes,...dc_heroes]
+console.log(all_new_heroes);
+
+
+const another_array = [1, [3, 2], 1,[ 2, 24, 34, 35], 3, 2, 5, [46,[3, 42, 425]]]
+console.log(another_array);
+const real_another_array = another_array.flat(3)
+
+console.log(real_another_array);
+
+
+console.log((Array.isArray('Farhan'))); // false
+console.log((Array.from('Farhan')));   // [ 'F', 'a', 'r', 'h', 'a', 'n' ]
+console.log((Array.from({ name: 'Hitesh' }))); // []
+
+const score1 = 100
+const score2 = 200
+const score3 = 300
+
+console.log(Array.of(score1, score2, score3)); // [100, 200, 300]
+*/
+
