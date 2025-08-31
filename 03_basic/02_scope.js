3d2818b6c5aea8ac753a49ab518f7532e8643c87
FARHAN AHMAD <113082273+farahn0104@users.noreply.github.com>
2025-08-31 11:50:35 +0000

    scope is done

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -31,3 +31,57 @@ function addOne(num) {
 const addTwo = function (num) { // expression 
     return num + 1
 }
+
+
+/* 
+
+Global Execution Context
+Node: Global Object
+
+Global Execution Context
+Browser: window object 
+
+***Javascript Execution Context***
+1.Global Execution Context :
+
+Exmaple of Code: 
+
+let val1 = 10;
+let val2 = 5;
+function addNum(num1,num2) {
+    let total = num1 + num2
+    return total;
+}
+let result1 = addNum(val1,val2)
+let result2 = addNum(10,2)
+
+2. Function Execution Context :
+
+3. Eval Execution Context :
+
+## How the Execution context is Execute: 
+first is   : Global execution context is created 
+seconds is : Memory Creation phase : sirf jagah allocate ki jaati hai variables ko
+
+# there val1 = undefined; // stored in Memory Creation phase
+# there val2 = undefined; // stored in Memory Creation phase
+
+# addNum = function definition as it is 
+result1 = undefined; // stored
+result2 = undefined; // stored
+
+Note: Cycle one is done and second phase is start
+
+third is : Execution phase :
+now assign the value in execution phase
+val1 = 10
+val2 = 5
+
+then  result1 create a our new execution context
+and create variable environment and execution thread
+then again memory phase and execution phase start for result1
+
+then  result2 create a our new execution context
+and create variable environment and execution thread
+then again memory phase and execution phase start for result2
+*/
